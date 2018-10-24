53a2103546549bf4c0475760d51916f4d7cdf9aa
Ammar The Trainer <ammarthetrainer@gmail.com>
2018-10-24 16:35:51 +0500

    final code upated from head office

---
+++ Ch11-P7-AnonymousTypes/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch11_P7_AnonymousTypes
+{
+    class Student
+    {
+        public int StudentID { get; set; }
+        public string StudentName { get; set; }
+        public int age { get; set; }
+    }
+}
